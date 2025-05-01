82a914f1fa9efc8eddb9edc71812a6c7fd68690a
Gabriel <gabrielsmain+git@gmail.com>
2025-05-01 16:30:06 +0200

    Delete main/java directory

--- main/java/Programmieraufgabe2.java
+++
@@ -1,6 +0,0 @@
-public class Programmieraufgabe2 {
-    public static void main(String[] args) {
-        String text = "Hello, world!\n" + "What a great day to be alive!";
-        System.out.println(text);
-    }
-}
