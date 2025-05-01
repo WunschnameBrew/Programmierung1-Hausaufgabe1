8078c41bb734a4ad6592a0133196f8427e669903
Gabriel <gabrielsmain+git@gmail.com>
2025-05-01 16:41:40 +0200

    Fertig

--- src/main/java/Programmieraufgabe2.java
+++ src/main/java/Programmieraufgabe2.java
@@ -1,6 +1,6 @@
 public class Programmieraufgabe2 {
     public static void main(String[] args) {
-        String text = "Hello, world!\n" + "What a great day to be alive!";
+        String text = "Hello, world! \n" + "What a great day to be alive!";
         System.out.println(text);
     }
 }
